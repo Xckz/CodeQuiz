25e4f7129418f951e1c463dfec747e69d95cb2aa
Lyman Connor <eckzly@gmail.com>
2021-09-06 18:38:45 -0400

    readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Code Quiz
+
+This is a quiz that will test you on various coding languages (HTML, CSS, JavaScript, etc).
+
+## How to Begin
+
+Click on the **Begin Quiz** button to start the quiz and begin answering questions. You will have **75 seconds** to finish the quiz. Selecting a correct answer will show that you're right beneath the question. If you answer the question incorrectly you will be told so and 10 seconds will also be taken off of the timer. When you finish the quiz you will be given the option to enter your name onto a highscore board. You can reset the highscore board if you'd like to by hitting the **Reset** button.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- assets/javascript.js
+++ assets/javascript.js
@@ -1,3 +1,104 @@
 //Required Variables
 
 let start_button = document.querySelector(".start-button button");
+let start_screen = document.querySelector(".start-screen");
+
+start_button.onclick = () => {
+    start_screen.classList.add("activeInfo");
+};
+
+exit_button.onclick = () => {
+    start_screen.classList.remove("activeInfo");
+};
+
+continue_button.onclick = () => {
+    start_screen.classList.remove("activeInfo");
+};
+
+let question_number = 0;
+
+function showQuestions() {
+    let questionText = document.querySelector(".answerTextf");
+    let que_tag = "<span" + questions[0].question + "</span>";
+    questionText.innerHTML = que_tag;
+}
+
+var questions = [
+    {
+        numb: 1,
+        question:
+            "Where in the HTML document should you link an external stylesheet?",
+        answer: "2. In the head",
+        options: [
+            "1. In the head",
+            "2. In the header",
+            "3. In the foot",
+            "4. In the footer",
+        ],
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "4. Cascading Style Sheets",
+        options: [
+            "1. Computerized Styling Standards",
+            "2. Cascading Styling Standard",
+            "3. Creative Style Source",
+            "4. Cascading Style Sheets",
+        ],
+    },
+    {
+        numb: 3,
+        question: "What are arrays used for?",
+        answer: "3. They are used for storing multiple values in a single variable",
+        options: [
+            "1. They are used to choose colors",
+            "2. They are used for changing multiple colors with a single variable",
+            "3. They are used for storing multiple values in a single variable",
+            "4. They are used for storing multiple colors",
+        ],
+    },
+    {
+        numb: 4,
+        question:
+            "In Bootstrap, which class provides a responsibe fixed width container?",
+        answer: "4. .container-fluid",
+        options: [
+            "1. .container-fixed",
+            "2. .container-reponsive",
+            "3. .container-rfw",
+            "4. .container-fluid",
+        ],
+    },
+    {
+        numb: 5,
+        question: "The Bootstrap grid system is based on how many columns?",
+        answer: "1. 12",
+        options: ["1. 12", "2. 9", "3. 6", "4. 3"],
+    },
+    {
+        numb: 6,
+        question:
+            "If an image can't be displayed, which HTML attribute specifies an alternate text for the image?",
+        answer: "4. alt",
+        options: ["1. descr", "2. src", "3. spec", "4. alt"],
+    },
+    {
+        numb: 7,
+        question: "Which attribute is used for a navigation bar?",
+        answer: "3. nav",
+        options: ["1. navbar", "2. navi", "3. nav", "4. nvgtn"],
+    },
+    {
+        numb: 8,
+        question:
+            "Which of the following is the best resource for information regarding Web Development?",
+        answer: "2. MDN Web Docs",
+        options: [
+            "1. Spider Worlds",
+            "2. MDN Web Docs",
+            "3. Wikipedia",
+            "4. Facebook",
+        ],
+    },
+];
